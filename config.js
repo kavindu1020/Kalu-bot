4b3624816b7fc0d7a5be4a7b86f2746c32205e8e
kavindu1020 <induwarak44@gmail.com>
2025-03-24 23:21:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.evn.ALIVE_IMG || "https//avatars.githubsercontent.com/u/106251140?v4",
+ALIVE_MSG: process.evn.ALIVE_MSG || "HELLO .I am kalu md I am alive now!".
 };
